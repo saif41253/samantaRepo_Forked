0644e82067a5c026692add7773f88dcdd955bc7c
arunk <arunk@Arun>
2025-02-28 13:24:46 +0530

    Add new contact and organisation details

--- DeepakAutomation/src/test/java/contactModule/CreateContact.java
+++ DeepakAutomation/src/test/java/contactModule/CreateContact.java
@@ -4,6 +4,9 @@ public class CreateContact {
 	
 	public static void main(String[] args) {
 		System.out.println("contact got created");
+		
+		
+		System.out.println(" all contact");
 	}
 
 }
--- DeepakAutomation/src/test/java/organisationModule/CreateOrganisation.java
+++ DeepakAutomation/src/test/java/organisationModule/CreateOrganisation.java
@@ -5,6 +5,8 @@ public class CreateOrganisation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("organisation got created");
+		
+		System.out.println("Create complete org");
 
 	}
 
