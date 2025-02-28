1a7a32451be26c48511be737014a07a5f44c2331
arunk <arunk@Arun>
2025-02-28 13:13:39 +0530

    New Project

---
+++ DeepakAutomation/src/test/java/contactModule/CreateContact.java
@@ -0,0 +1,9 @@
+package contactModule;
+
+public class CreateContact {
+	
+	public static void main(String[] args) {
+		System.out.println("contact got created");
+	}
+
+}
---
+++ DeepakAutomation/src/test/java/organisationModule/CreateOrganisation.java
@@ -0,0 +1,11 @@
+package organisationModule;
+
+public class CreateOrganisation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("organisation got created");
+
+	}
+
+}
